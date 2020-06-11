cbea43ede4a6a85d24dd7227b3e66e60967aa99f
Omer Waleed <omerwaleed712@gmail.com>
2020-06-11 13:05:33 +0500

    folders initial CRUD with user authorization 2

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,4 +1,6 @@
 class Stock < ApplicationRecord
   belongs_to :category
   belongs_to :sub_category
+
+  mount_uploaders :images, ImageUploader
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200521023732) do
+ActiveRecord::Schema.define(version: 20200516041734) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,14 +30,6 @@ ActiveRecord::Schema.define(version: 20200521023732) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "design_by_user"
-  end
-
-  create_table "folders", force: :cascade do |t|
-    t.string "name"
-    t.bigint "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_folders_on_user_id"
   end
 
   create_table "images", force: :cascade do |t|
@@ -105,5 +97,4 @@ ActiveRecord::Schema.define(version: 20200521023732) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "folders", "users"
 end
