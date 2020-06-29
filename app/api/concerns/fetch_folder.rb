b49ae0b51d573dee126019839f0aeba50ea93020
Omer Waleed <omerwaleed712@gmail.com>
2020-06-29 14:42:36 +0500

    changes in folders design and uplaod response

--- app/api/concerns/fetch_folder.rb
+++ app/api/concerns/fetch_folder.rb
@@ -11,8 +11,8 @@ module FetchFolder
                 name: folder.name,
                 parent_id: folder.parent_id,
                 subfolders: serialize_collection(folder.subfolders, serializer: FolderSerializer),
-                designs: folder.containers.where(instance_type: 'Design').select{ |container| container.instance.is_trashed == 0 }.map { |container| {container_id: container.id, design_id: container.instance.id, images: serialize_collection(container.instance.images, serializer: ImageSerializer) } },
-                uploads: folder.containers.where(instance_type: 'Upload').select{ |container| container.instance.is_trashed == 0 }.map { |container| {container_id: container.id, upload_id: container.instance.id, images: serialize_collection(container.instance.images, serializer: ImageSerializer) } },
+                designs: folder.containers.where(instance_type: 'Design').select{ |container| container.instance.is_trashed == 0 }.map { |container| DesignSerializer.new(container.instance)},
+                uploads: folder.containers.where(instance_type: 'Upload').select{ |container| container.instance.is_trashed == 0 }.map { |container| UploadSerializer.new(container.instance) }
             } }
             return folders
         end
