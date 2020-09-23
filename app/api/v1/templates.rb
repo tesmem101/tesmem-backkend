d637819cd52687ae1816fe679aa3019394da76e8
Omer Waleed <omerwaleed712@gmail.com>
2020-09-24 02:19:55 +0500

    all cats all sub cats when no cat id

--- app/api/v1/templates.rb
+++ app/api/v1/templates.rb
@@ -3,6 +3,7 @@ module V1
     include AuthenticateRequest
     include V1Base
     include FetchTemplate
+    include FetchCategories
     version "v1", using: :path
 
     resource :templates do
@@ -11,10 +12,17 @@ module V1
            { consumes: ['application/x-www-form-urlencoded'],
              http_codes: [{ code: 200, message: 'success' }] }
       get '/' do
-        cat_id = params['category_id']
+        cat_id = params['category_id'].present? ? params['category_id'] : nil
         search = params['search'].present? ? params['search'].downcase : nil
-        templates = Category.find(cat_id).sub_categories.where("lower(title) LIKE ?", "%#{search}%").includes(:designers).all.map { |sub_c| get_template(sub_c) }
-        render_success(templates.as_json)
+        if cat_id
+          templates = Category.find(cat_id).sub_categories.where("lower(title) LIKE ?", "%#{search}%").includes(:designers).all.map { |sub_c| get_template(sub_c) }
+          render_success(templates.as_json)
+        else
+          templates = all_categories.includes(:sub_categories).all.map {|cat| 
+            cat.sub_categories.where("lower(title) LIKE ?", "%#{search}%").includes(:designers).all.map { |sub_c| get_template(sub_c) }
+          }.flatten
+          render_success(templates.as_json)
+        end
       end
 
     end
