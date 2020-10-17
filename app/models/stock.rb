94798ec966647417372602f462b3f1d1b8c93ef2
Omer Waleed <omerwaleed712@gmail.com>
2020-10-17 16:30:52 +0500

    svg ids an colors

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,5 +1,6 @@
 class Stock < ApplicationRecord
   include CarrierWave::MiniMagick
+  include Nokogiri
   
   belongs_to :category
   belongs_to :sub_category
@@ -11,6 +12,26 @@ class Stock < ApplicationRecord
   
   after_find :mapping_image_url
   after_save :update_url
+  before_save :add_ids_to_svg
+
+  def add_ids_to_svg
+    if self.svg.present?
+      url = "#{ENV["RAIL_SERVER_URL"]}#{self.svg}"
+      doc = Nokogiri::HTML open(url)
+      svg = doc.at_css 'svg'
+      count = 1
+      colors = []
+      svg.xpath('//path').each{|tag| 
+          tag['id'] = "PATH_#{count}"
+          count = count + 1
+          colors.push({
+            id: tag['id'],
+            color: tag['fill']
+          })
+      }
+      self.colors = colors.to_json
+    end
+  end
 
   def mapping_image_url
     if self.image.present?
