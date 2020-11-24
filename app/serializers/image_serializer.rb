5812f943690d0abc448cf4a470667c57633bc253
Atif <atif@tesmem.com>
2020-11-25 00:38:32 +0500

    add image version urls

--- app/serializers/image_serializer.rb
+++ app/serializers/image_serializer.rb
@@ -1,3 +1,12 @@
 class ImageSerializer < ActiveModel::Serializer
-  attributes :name, :description, :url, :height, :width
+  attributes :name, :description, :url, :height, :width, :default_url, :thumb_url
+
+  def default_url
+    object.image.image.url rescue '---'
+  end
+
+  def thumb_url
+    object.image.image.thumb.url  rescue '---'
+  end
+
 end
--- app/serializers/upload_serializer.rb
+++ app/serializers/upload_serializer.rb
@@ -1,6 +1,6 @@
 class UploadSerializer < ActiveModel::Serializer
   attributes :id, :title, :is_trashed, :images, :container, :user
   has_one :container
-  has_many :images
+  has_many :images, serializer: ImageSerializer
   belongs_to :user
 end
