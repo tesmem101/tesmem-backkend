6994aea0b60014e4db5304905ed1950dbc7e3050
Atif <atif@tesmem.com>
2020-12-07 10:12:30 +0500

    update

--- restart_server.sh
+++ restart_server.sh
@@ -2,14 +2,14 @@ RAILS_ENV=production
 # 1) Installing missing gems
 bundle install
 # 2) Precmpining Assets
-bundle exec rake assets:precompile #RAILS_ENV=${RAILS_ENV}
+RAILS_ENV=${RAILS_ENV} bundle exec rake assets:precompile
 # 3) Stop Puma Server
 # kill -9 $(lsof -i tcp:3000 -t)
 PID=`pgrep -f '^([^ ]*/)?puma '`
 kill -9 $PID
 
 # 4) Database
-bundle exec rake db:migrate
+RAILS_ENV=${RAILS_ENV} bundle exec rake db:migrate
 
 # 5) Start Puma Server
 puma -C config/puma.rb -e ${RAILS_ENV} -d
