bfb87f679900016b3aadc2a23ea45af094331dd6
Atif <atif@tesmem.com>
2020-12-18 13:03:18 +0500

    update

--- app/serializers/intermediate_category_serializer.rb
+++ app/serializers/intermediate_category_serializer.rb
@@ -1,5 +1,5 @@
 class IntermediateCategorySerializer < ActiveModel::Serializer
-  attributes :id, :title, :title_ar, :super_category_id, :image, :intermediate_categories, :width, :height, :unit, :designers
+  attributes :id, :title, :title_ar, :super_category_id, :width, :height, :unit
   has_many :sub_categories
   has_many :stocks
 end
