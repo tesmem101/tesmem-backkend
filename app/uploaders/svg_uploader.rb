f288739b37542e7e319ad1391fb386749240d163
Atif <atif@tesmem.com>
2021-01-28 17:56:06 +0500

    add thumbnail version

--- app/uploaders/svg_uploader.rb
+++ app/uploaders/svg_uploader.rb
@@ -5,6 +5,10 @@ class SvgUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
+  version :thumb do
+    process resize_to_fit: [300, 300]
+  end
+
   def extension_whitelist
     [:svg]
   end
