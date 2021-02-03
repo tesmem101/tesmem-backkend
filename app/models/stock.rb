d1a22b247c932d5213aca507ba66d305536f20c5
Atif <atif@tesmem.com>
2021-02-03 15:47:03 +0500

    changes for svg_thumb

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -6,7 +6,7 @@ class Stock < ApplicationRecord
 
   mount_uploader :image, StockUploader
   mount_uploader :svg, SvgUploader
-  mount_uploader :svg_thumb, SvgUploader
+  mount_uploader :svg_thumb, SvgThumbUploader
 
   belongs_to :category
   belongs_to :sub_category
--- app/serializers/stock_serializer.rb
+++ app/serializers/stock_serializer.rb
@@ -1,4 +1,4 @@
 class StockSerializer < ActiveModel::Serializer
   attributes :id, :title, :title_ar, :description, :url, :stocktype,
-  :category_id, :sub_category_id, :json, :specs, :created_at, :image, :svg
+  :category_id, :sub_category_id, :json, :specs, :created_at, :image, :svg, :svg_thumb
 end
