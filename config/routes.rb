ab5f0ec61eb06cda63cf928ca366f425803e7b38
Atif <atif@tesmem.com>
2021-02-06 17:43:49 +0500

    Disable SignUp functionality for Admin User

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'home#index'
 
-  devise_for :users
+  devise_for :users, skip: [:registrations]
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   mount ApplicationApi, at: '/'
 end
