20a55de357fa0f9efdd13aa65b35b716a2df7f48
arslan ahmad <arsalan@tesmem.com>
2021-02-24 17:59:53 +0500

    remove json attribute in response of get tamplates api and change completly response in case user search any specific template by keyword

--- app/api/v1/templates.rb
+++ app/api/v1/templates.rb
@@ -20,8 +20,13 @@ module V1
         cat_id = params['category_id'].present? ? params['category_id'] : nil
         search = params['search'].present? ? params['search'].downcase : nil
         locale = params['locale'].present? ? "_#{params['locale']}" : ""
-        if cat_id
-          templates = Category.find(cat_id).sub_categories.search_keyword(locale, search).includes(:designers).all.map { |sub_c| get_template(sub_c) }
+
+        if cat_id && search
+          templates_ids = Category.find(cat_id).sub_categories.joins(:designers).joins("inner join designs on designs.id = designers.design_id ").where("lower(designs.title#{locale}) LIKE ?", "%#{search}%").select("designs.id")
+          templates = Design.where(id: templates_ids).collect{ |design| make_design_object(design) }
+          render_success(templates.as_json)          
+        elsif cat_id
+          templates = Category.find(cat_id).sub_categories.search_keyword(locale, search).includes(:designers).all.map { |sub_c| get_template(sub_c) } # Previous One 
           render_success(templates.as_json)
         else
           templates = all_categories.includes(:sub_categories).all
