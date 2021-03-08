ad4cc565154ad49a984f56a9459b7ddfa06e50ee
arslan ahmad <arsalan@tesmem.com>
2021-03-08 14:57:34 +0500

    arrange and hide fields of stock resource of admin panel

--- app/models/concerns/stock_admin.rb
+++ app/models/concerns/stock_admin.rb
@@ -7,6 +7,61 @@ module StockAdmin
         configure :description do
           hide
         end
+        configure :json do
+          hide
+        end
+        configure :url do
+          hide
+        end
+        configure :specs do
+          hide
+        end
+        configure :created_at do
+          hide
+        end
+        configure :updated_at do
+          hide
+        end
+      end
+      show do
+        configure :description do
+          hide
+        end
+        configure :json do
+          hide
+        end
+        configure :url do
+          hide
+        end
+        configure :specs do
+          hide
+        end
+        configure :created_at do
+          hide
+        end
+        configure :updated_at do
+          hide
+        end
+      end
+      edit do
+        configure :description do
+          hide
+        end
+        configure :json do
+          hide
+        end
+        configure :url do
+          hide
+        end
+        configure :specs do
+          hide
+        end
+        configure :created_at do
+          hide
+        end
+        configure :updated_at do
+          hide
+        end
       end
     end
   end
