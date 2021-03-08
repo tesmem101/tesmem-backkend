cb16e53c40cd61cca16511f32e6a6db09e2057ed
arslan ahmad <arsalan@tesmem.com>
2021-03-08 12:30:10 +0500

    pagination on unsplash api applied

--- app/api/v1/unsplash.rb
+++ app/api/v1/unsplash.rb
@@ -8,15 +8,20 @@ module V1
         desc 'Get all unsplash images',
              { consumes: ['application/x-www-form-urlencoded'],
                http_codes: [{ code: 200, message: 'success' }] }
+        params do
+          requires :page, type: String, :desc => 'Page Number'
+          requires :per_page, type: Integer, :desc => 'Number of elements on each page'
+        end
         get '/' do
-          search = params['search'].present? ? params['search'].downcase : nil
-          unsplash_images = []
-          page_limit = 3
-          for page_number in 1..page_limit do
-            unsplash_images.concat get_unsplash_images(search, page_number, nil, 30, 'latest').map { |photo| map_unsplash_images(photo.table, 'regular') }
+          if params[:page].present? && params[:per_page].present?
+            search = params['search'].present? ? params['search'].downcase : nil
+            unsplash_images = []
+            unsplash_images.concat get_unsplash_images(search, params[:page], nil, params[:per_page], 'latest').map { |photo| map_unsplash_images(photo.table, 'regular') }
+            records = get_unsplash_response(unsplash_images)
+            render_success(records.as_json)
+          else
+            render_error(RESPONSE_CODE[:bad_request], 'Sorry! You should pass page and per_page params!')
           end
-          records = get_unsplash_response(unsplash_images)
-          render_success(records.as_json)
         end
       end
     end
